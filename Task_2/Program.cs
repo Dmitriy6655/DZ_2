599fda9ec52855d6d07a2ab9b651f7503285ab32
Timofey_V <tim_vi_ov@mail.ru>
2022-11-27 09:46:08 +0300

    add folder Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит третью 
+//цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число");
+long numberA = long.Parse(Console.ReadLine());
+long result = 0;
+
+if(numberA < 100)
+{
+ Console.WriteLine("Третей цифры нет");  
+}
+
+while(numberA > 100 || numberA == 100)
+{
+    if(numberA < 1000)
+    {
+    result = numberA % 10;
+    Console.WriteLine($"Третья цифра трехзначного числа равна: {result}");
+    break;
+    }
+   
+    else
+    {
+    numberA = numberA / 10;
+    }
+
+};
+
+
+
+
+
