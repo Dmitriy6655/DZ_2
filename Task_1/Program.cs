33d60fbd4ee237d27316d15e2ff3c6c20d15ad3c
Timofey_V <tim_vi_ov@mail.ru>
2022-11-27 09:45:53 +0300

    add folder Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+int numberA = int.Parse(Console.ReadLine());
+int number_2;
+if(numberA >= 100 & numberA <= 999 )
+{
+number_2  = numberA / 10;
+number_2 = number_2 % 10;
+Console.WriteLine($"Вторая цифра трехзначного числа равна: {number_2}");
+}
+
+if (numberA == 0 || numberA <= 99 || numberA >= 1000)
+{
+ Console.WriteLine("Число не 3-х значное!");   
+};
+
+
+
